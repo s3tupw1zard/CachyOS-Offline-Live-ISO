29fd50ededa59aaefbb20e01abcbbbe4b7c2a858
s3tupw1zard <50152835+s3tupw1zard@users.noreply.github.com>
2025-01-09 09:59:23 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+These are the basic needed files and folders to build CachyOS system.
+
+### buildiso
+
+buildiso is used to build CachyOS ISO.
+
+#### Arguments
+
+~~~
+$ ./buildiso.sh -h
+Usage: buildiso [options]
+    -c                 Disable clean work dir
+    -h                 This help
+    -p <profile>       Buildset or profile [default: kde]
+    -v                 Verbose output to log file, show profile detail (-q)
+~~~
+
+* Uses the same signature that normal repo and has no mirrors package to install.
+
+```bash
+sudo pacman -Syy
+```
+
+### Install necessary packages:
+```bash
+sudo pacman -S archiso mkinitcpio-archiso git squashfs-tools --needed
+```
+
+### Clone:
+```bash
+git clone https://github.com/cachyos/cachyos-live-iso.git cachyos-archiso
+cd cachyos-archiso
+```
+
+### Build
+```bash
+sudo ./buildiso.sh -p kde -v
+```
+
+As the result iso appears at the `out` folder
---
+++ archiso/airootfs/usr/local/bin/calamares-online.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+FollowFile() {
+    local tailfile="$1"
+    local term_title="$2"
+
+    alacritty -t "$term_title" -e tail -f "$tailfile" &
+}
+
+catch_chrooted_pacman_log() {
+    local pacmanlog=""
+    local lockfile="$HOME/.$1.lck"
+
+    # wait until pacman.log is available in the chrooted system, then follow the log in background
+    while true ; do
+        sleep 2
+        pacmanlog="$(/usr/bin/ls -1 /tmp/calamares-root-*/var/log/pacman.log 2>/dev/null | /usr/bin/tail -n 1)"
+        if [ -n "$pacmanlog" ] ; then
+            # pacman.log found
+            [ -r "$lockfile" ] && return
+            /usr/bin/touch "$lockfile"
+            FollowFile "$pacmanlog" "Pacman log" 400 50
+            break
+        fi
+    done
+}
+
+Main() {
+    local progname
+    progname="$(basename "$0")"
+    local log=/home/liveuser/cachy-install.log
+    local mode=""
+
+    case "$progname" in
+        calamares-online.sh) mode=online ;;
+        calamares-offline.sh) mode=offline ;;
+    esac
+    mode=online  # keep this line for now!
+
+    BOOTLOADER="SYSTEMD-BOOT Deckify"
+    echo "USING SYSTEMD-BOOT-DECKIFY!"
+    yes | sudo pacman -R cachyos-calamares-qt6-next-deckify
+    yes | sudo pacman -Sy cachyos-calamares-qt6-next-deckify
+
+    cat <<EOF > $log
+########## $log by $progname
+########## Started (UTC): $(date -u "+%x %X")
+########## Install mode: $mode
+########## System: $SYSTEM
+########## Bootloader: $BOOTLOADER
+EOF
+    FollowFile "$log" "Install log" 20 20
+
+    sudo cp /usr/share/calamares/settings_${mode}.conf /etc/calamares/settings.conf
+    sudo -E  dbus-launch calamares -D6 >> $log &
+
+    # comment out the following line if pacman.log is not needed:
+    [ "$mode" = "online" ] && catch_chrooted_pacman_log "$progname"
+}
+
+Main "$@"
